11b66e8863cdc0f2f2dfc98d0e7133bbe44a9da5
Harkanwal Gill <harkanwalsgill@gmail.com>
2025-02-21 12:07:25 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,15 @@ variable "random_id_count" {
   type    = number
 }
 
+variable "random_name" {
+  type    = string
+}
+
 resource "random_id" "this" {
   count       = var.random_id_count
   byte_length = 16
+}
+
+output "random_name" {
+  value = var.random_name
 }
